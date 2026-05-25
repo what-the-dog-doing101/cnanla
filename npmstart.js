2850bd7c478cb1b48570354b0c31de2dfc176108
what-the-dog-doing101 <matthew.gable14@gmail.com>
2026-05-25 15:07:54 -0400

    Update npmstart.js

--- npmstart.js
+++ npmstart.js
@@ -1,33 +1,23 @@
-(function () {
-  function loadScript(src, onLoad, onError) {
-    const s = document.createElement("script");
-    s.src = src;
-    s.onload = onLoad;
-    s.onerror = onError;
-    document.body.appendChild(s);
-  }
+console.log("ran npmstart")
 
-  function start() {
-    loadScript(
+(function(){
+
+function js(src,cb){
+  const s=document.createElement("script");
+  s.src=src;
+  s.onload=cb;
+  document.head.appendChild(s);
+}
+
+js(
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js",
+  ()=>js(
+    "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore-compat.js",
+    ()=>js(
       "https://what-the-dog-doing101.github.io/cnanla/loader.js",
-      () => console.log("ANALYTICS READY"),
-      (e) => console.log("ANALYTICS FAILED", e)
-    );
-  }
+      ()=>console.log("ANALYTICS READY")
+    )
+  )
+);
 
-  if (window.firebase) {
-    start();
-  } else {
-    loadScript(
-      "https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js",
-      () => {
-        loadScript(
-          "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore-compat.js",
-          start,
-          (e) => console.log("FIRESTORE LOAD FAILED", e)
-        );
-      },
-      (e) => console.log("FIREBASE LOAD FAILED", e)
-    );
-  }
 })();
